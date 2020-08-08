23950103b32c09b05014a370f19a414a78b1eb0e
j <joelsouthgate@googlemail.com>
2020-08-08 17:49:08 +0100

    updated core alg for 12 arrays

--- uk2.cpp
+++ uk2.cpp
@@ -41,113 +41,142 @@ double betap(d,M,N,k,a=1,b=1) {
     }
 } 
 
+int cal_imax(d, m, n) {
+    if (m >= n) return d <= 0 ? m : m-d;
+    int v = n-m;
+    return d <= v ? m : m-d+v;
+}
+
+std::vector<int> cal_imax_arr(m,n) {
+    std::vector<int> res;
+    for (int ld = 0; ld < m+n+1; ++ld) {
+        res.push_back(cal_imax(ld-m,m,n);
+    }
+    return res;
+}
+
+/*
+* Compute the diagonal dynamic programming matrix and auxiliaries
+*
+*
+*/
 bool DistCalculator::calculate_dist(std::string s1, std::string s2, int* state_triple, int* state_arr, int rowsize, int snpmax, int slide_threshold, bool freeze) {
     int m = s1.length();
     int n = s2.length();
-    // make sure frozen not true
-    // init
     int h_start = state_triple[0];
     int lower_bound = state_triple[1];
     int upper_bound = state_triple[2];
+    // Init L arrays
     auto L0 = state_arr;
     auto L1 = state_arr + rowsize;
     auto L2 = state_arr + 2*rowsize;
+    // Init M arrays; number of mismatch operations
     auto M0 = state_arr + 3*rowsize;
     auto M1 = state_arr + 4*rowsize;
     auto M2 = state_arr + 5*rowsize;
-//    std::cerr << "RESUMING:" << "h=" << h_start << ",lb=" << lower_bound << ",ub=" << upper_bound << std::endl; 
-//    std::cerr << "m n" << m << " " << n << std::endl;
-//    std::cerr << "FREEZE?" << freeze << std::endl;
-//    cerrarr(L0, rowsize);
-//    cerrarr(L1, rowsize);
-//    cerrarr(L2, rowsize);
-//    std::cerr << std::endl;
-//    cerrarr(M0, rowsize);
-//    cerrarr(M1, rowsize);
-//    cerrarr(M2, rowsize);
+    // Auxiliary arrays:
+    // Init NM arrays; number of match operations
+    auto NM0 = state_arr + 3*rowsize;
+    auto NM1 = state_arr + 4*rowsize;
+    auto NM2 = state_arr + 5*rowsize;
+    // Init NN arrays; number of `N' match operations
+    auto NN0 = state_arr + 3*rowsize;
+    auto NN1 = state_arr + 4*rowsize;
+    auto NN2 = state_arr + 5*rowsize;
 
     if (freeze && n == 0) { return false; }
 
     int h = h_start;
+    std::vector<int> imax_arr = cal_imax_arr(m, n);
     while (h < 2*(m+n)+1) {
         state_triple[0] = h;
-//    for (int h = h_start; h < 2*(m+n)+1; ++h) {
-        // printarr
-//        std::cout <<  "h=" << h << " " << std::endl;
-//        std::cout << std::max(-h/2, lower_bound) << " " << h << " " << std::min(h/2 + 1, upper_bound+1) << std::endl;
-//        cerrarr(L0, rowsize);
-//        cerrarr(L1, rowsize);
-//        cerrarr(L2, rowsize);
-//        std::cerr << std::endl;
-//        cerrarr(M0, rowsize);
-//        cerrarr(M1, rowsize);
-//        cerrarr(M2, rowsize);
-//        std::cerr << std::endl;
         int prev_lower_bound = lower_bound;
         int prev_upper_bound = upper_bound;
-        for (int d = std::max(-(h/2), lower_bound); d < std::min((h/2)+1,upper_bound+1); ++d) {
- //           std::cout << "\t" << lower_bound << " " << d << " " << upper_bound << std::endl;
+        for (int d = std::max(-(h/2), lower_bound); d < std::min((h/2),upper_bound)+1; ++d) {
             int ld = m+d;
+            int imax = imax_arr[ld];
+            assert (ld >= 0);
+            assert (lower_bound + m >= 0);
+            assert (upper_bound + m <= m+n+1);
+            assert (L1[ld] < imax);
             int maxi = -1;
             int lmove = L0[ld-1];
-            int matchmove = L1[ld] + 1;
+            int matchmove = std::min(L1[ld] + 1, imax);
+            int rmove = std::min(L0[ld+1] + 1, imax);
             int prev_mm = M1[ld];
-            int rmove = -1;
+            int prev_NM = -1;
+            int prev_NN = -1;
+            // TODO: check the bounds are not redundant
             if ((d > -h) && (ld-1 > 0)) {
                 maxi = std::max(maxi, lmove);
                 M2[ld] = M0[ld-1];
+                prev_NM = NM0[ld-1];
+                prev_NN = NN0[ld-1];
             }
             if ((d < h) && (ld+1 < m+n+1)) {
-                rmove = L0[ld+1] + 1;
                 maxi = std::max(maxi, rmove);
-                if (rmove >= lmove) { M2[ld] = M0[ld+1]; }
+                if (rmove >= lmove) { 
+                    M2[ld] = M0[ld+1]; 
+                    prev_NM = NM0[ld+1];
+                    prev_NN = NN0[ld+1];
+                }
             }
             if (matchmove >= maxi) {
                 maxi = matchmove;
+                prev_NM = NM1[ld];
+                prev_NN = NN1[ld];
                 if (maxi > 0) {
+                    prev_NM += 1;
                     M2[ld] = prev_mm + 1;
                 }
             }
-//            std::cout << "maxi" << maxi << std::endl;
-            L2[ld] = slide(d, maxi, s1, s2);
-            int imax = m - std::max(0, d-(n-m));
-            if (L2[ld] > imax) { L2[ld] = imax; }
+            // TODO: redundant?
+            maxi = std::min(maxi, imax);
+            int sl, mn;
+            if (maxi < imax) {
+                std::pair<int,int> = slide(d, maxi, s1, s2);
+                sl = side.first; mn = slide.second;
+                assert (sl <= imax);
+                prev_NM += (sl-maxi);
+                prev_NN += mn;
+                L2[ld] = sl;
+            }
+            else { L2[ld] = maxi; }
+            NM2[ld] = prev_NM;
+            NN2[ld] = prev_NN;
+            assert (maxi <= imax);
+            assert (L2[ld] <= imax);
+                    
             if (freeze && d >= n-m) {
                 if (L2[ld] == imax) {
-//                    std::cout << "freezing" << std::endl;
                     return true;
                 }
             }
+
             assert (M2[ld] >= 0);
-            if (d < 0 && L2[ld] >= m && d > lower_bound) {
-                lower_bound = d; state_triple[1] = d;
+            if (ld <= n && L2[ld] >= imax) {
+                lower_bound = d+1; state_triple[1] = d;
+                assert (lower_bound + m >= 0);
             }
-            if (d > 0 && L2[ld] >= n-d && d < upper_bound) {
+            if (ld > n && L2[ld] >= imax) {
                 upper_bound = d-1; state_triple[2] = d-1;
+                assert (upper_bound + m >= n);
+                assert (upper_bound + m <= n+m+1);
+                break;
             } 
+            if (L2[n] == m) {
+                return true;
+            }
         }
-        // TODO: figure out fastest way to copy/clear the three arrays
-//       cerrarr(L2);
-//        std::cout << std::endl;
-///        std::cout << "checking end" << L2[n] << "=? " << m << std::endl;
-//        std::cout << "n:" << n << std::endl;
-//        cerrarr(L0, rowsize);
-//       cerrarr(L1, rowsize);
-//        cerrarr(L2, rowsize);
-//        std::cout << std::endl;
-//        cerrarr(M0, rowsize);
-//        cerrarr(M1, rowsize);
-//        cerrarr(M2, rowsize);        
-        if (L2[n] == m) {
-//            std::cout << "ending early at h" << " " << h << std::endl;
-            return true;
-        }
-//        L0 = L1;
-//        std::cout << "memcopying" << std::endl;
+        // TODO: memcopy? don't overlap in range
         std::memmove(L0, L1, rowsize * sizeof(L0[0]));
         std::memmove(L1, L2, rowsize * sizeof(L0[0]));
         std::memmove(M0, M1, rowsize * sizeof(L0[0]));
         std::memmove(M1, M2, rowsize * sizeof(L0[0]));
+        std::memmove(NM0, NM1, rowsize * sizeof(L0[0]));
+        std::memmove(NM1, NM2, rowsize * sizeof(L0[0]));
+        std::memmove(NN0, NN1, rowsize * sizeof(L0[0]));
+        std::memmove(NN1, NN2, rowsize * sizeof(L0[0]));
         h += 1;
     }
     return true;
@@ -220,7 +249,7 @@ void DistCalculator::init_state_triple(int* state_triple, int len1, int len2) {
 void DistCalculator::query_samples_against_refs(std::string sample_fasta_fname, std::string ref_fasta_fname) {
     std::vector<std::pair<std::string, std::string>> queries = read_fasta(sample_fasta_fname);
     std::vector<std::pair<std::string, std::string>> refs = read_fasta(ref_fasta_fname);   
-    int state_arr[6*MAX_ROW_SIZE] = {0};
+    int state_arr[12*MAX_ROW_SIZE] = {0};
     int state_triple[3] = {0,0,0};
     for (auto& p1: queries) {
         for (auto& p2: refs) {
@@ -228,8 +257,12 @@ void DistCalculator::query_samples_against_refs(std::string sample_fasta_fname, 
             init_state_array(state_arr, rowsize);
             init_state_triple(state_triple, p1.second.length(), p2.second.length());
             int mm_ind = 5*rowsize + p2.second.length();
+            int NM_ind = 8*rowsize + p2.second.length();
+            int NN_ind = 11*rowsize + p2.second.length();
             calculate_dist(p1.second, p2.second, state_triple, state_arr, rowsize, 5, 100, false);
-            std::cout << p1.first << "," << p2.first << "," << state_triple[0] << "," << state_arr[mm_ind] << std::endl;
+            std::cout << p1.first << "," << p2.first << "," 
+                      << state_triple[0] << "," << state_arr[mm_ind] 
+                      << state_arr[NM_ind] << "," << state_arr[NN_ind] << std::endl;
         }
     }
 }
