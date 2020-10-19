1f8634d1621ee3e71d417d670bfc98b84b1df71a
j <joelsouthgate@googlemail.com>
2020-10-19 15:43:03 +0100

    comment

--- src/utilfunc.cpp
+++ src/utilfunc.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 
 extern "C" {
-    int max_match_ind(const char* a, const char* b) {
-//        std::cout << strlen(a) << " " << strlen(b) << std::endl;
-        for (int j = 0; j < std::min(strlen(a), strlen(b)); ++j) {
-//            std::cout << a[j] << " " << b[j] << std::endl;
-            if (a[j] != b[j]) return j-1;
+/**
+* Find the maximum matching index between two C strings.
+*
+* @param first the first string.
+* @param second the second string.
+* @return the maximum index that the strings match with.
+*/
+    int max_match_ind(const char* first, const char* second) {
+        for (int j = 0; j < std::min(strlen(first), strlen(second)); ++j) {
+            if (first[j] != second[j]) return j-1;
         }
         return strlen(a)-1;
     }
