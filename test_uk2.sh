c8cc1c8f8dcd587a072d9988ac27a41bb0a6778e
j <joelsouthgate@googlemail.com>
2020-10-19 15:34:52 +0100

    remove old files

--- test_uk2.sh
+++
@@ -1,2 +0,0 @@
-./main query_1.fa gisaid_hcov-19_2020_05_15_14.fa.h20000 > gisaid_hcov-19_2020_05_15_14.fa.h20000.csv 
-python3.7 test_uk2_out.py gisaid_hcov-19_2020_05_15_14.fa.h20000.m.cds.fa gisaid_hcov-19_2020_05_15_14.fa.h20000.csv
