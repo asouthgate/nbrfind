98342590404a36fa7b9eb275eb9900c7b2b07f55
j <joelsouthgate@googlemail.com>
2020-10-19 16:07:27 +0100

    fix infinite loop

--- src/probfunc.hpp
+++ src/probfunc.hpp
@@ -1,7 +1,10 @@
-#include "probfunc.hpp"
+#ifndef PROBFUNC_HPP
+#define PROBFUNC_HPP
 
 double binom(unsigned int n, unsigned int k);
 
 double logbeta(double u, double v);
 
 double betap(int d, int M, int N, int k, int a, int b);
+
+#endif
