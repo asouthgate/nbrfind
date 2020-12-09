424dda8d6397f853d5c02cc181cfb5035fb0277f
j <joelsouthgate@googlemail.com>
2020-12-09 18:57:54 +0000

    add option for k

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "uk2.hpp"
+#include <string>
 
 
 int main(int argc, char* argv[]) {
+    int snpmax = atoi(argv[3]);
     DistCalculator dc = DistCalculator();
-    dc.query_samples_against_refs(argv[1], argv[2]);
+    dc.query_samples_against_refs(argv[1], argv[2], snpmax);
 }
 
