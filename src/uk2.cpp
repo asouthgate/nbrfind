2c4ed04471c25a1c0530c9103f9d794a114e4dd1
j <joelsouthgate@googlemail.com>
2020-12-09 18:58:21 +0000

    some fixes in reporting

--- src/uk2.cpp
+++ src/uk2.cpp
@@ -47,7 +47,8 @@ MoveResult cal_move(StateData& sd, const int& d, const int& ld, const int& m, co
     int matchmove = std::min(sd.L1[ld] + 1, imax);
     int rmove = std::min(sd.L0[ld+1] + 1, imax);
     int prev_mm = sd.M1[ld];
-    int maxi, prev_NM, prev_NN;
+    int maxi = -1;
+    int prev_NM, prev_NN;
     // TODO: check the bounds are not redundant
     if ((d > -sd.h) && (ld - 1 > 0)) {
         maxi = std::max(maxi, lmove);
@@ -74,6 +75,7 @@ MoveResult cal_move(StateData& sd, const int& d, const int& ld, const int& m, co
     }
     // TODO: redundant?
     maxi = std::min(maxi, imax);
+    std::cerr << "maxi: " << maxi << std::endl;
     return MoveResult(maxi, prev_NM, prev_NN);
 }
 
@@ -88,7 +90,8 @@ bool DistCalculator::calculate_dist_sd(std::string s1, std::string s2, StateData
     fill_imax_arr(imax_arr, m, n);
     // Why is this maxh?
     int maxh = (2 * (m + n) + 1);
-    while ( (sd.h) < maxh) {
+    while ((sd.h) < maxh) {
+        sd.print_debug();
         // Why do we store prev_lower and prev_upper?
         int prev_lower_bound = sd.lower_bound;
         int prev_upper_bound = sd.upper_bound;
@@ -104,9 +107,11 @@ bool DistCalculator::calculate_dist_sd(std::string s1, std::string s2, StateData
         // Why is this dmax?
         int dmax = std::min( (sd.h/2), sd.upper_bound) + 1;
         for (int d = dstart; d < dmax; ++d) {
+            std::cerr << d << std::endl;
             // Get diagonal index
             int ld = m + d;
             // Why? what is imax?
+            // imax is the maximum i that a diagonal can get to
             int imax = imax_arr[ld];
             assert (ld >= 0);
             assert (sd.lower_bound + m >= 0);
@@ -126,7 +131,6 @@ bool DistCalculator::calculate_dist_sd(std::string s1, std::string s2, StateData
             
             // MOVE 2: slide
             int sl, mn; 
-            // What is maxi vs imax??
             if (move_result.maxi < imax) {
                 std::pair<int,int> res =  slide(d, move_result.maxi, s1, s2, imax);
                 sl = res.first; mn = res.second;
@@ -172,6 +176,7 @@ bool DistCalculator::calculate_dist_sd(std::string s1, std::string s2, StateData
                 return true;
             }
         }
+        sd.print_debug();
 
         // SWAP POINTERS
         sd.swap_pointers();
@@ -183,19 +188,24 @@ bool DistCalculator::calculate_dist_sd(std::string s1, std::string s2, StateData
 void DistCalculator::query_samples_against_refs(std::string sample_fasta_fname, std::string ref_fasta_fname, int k, double epsilon) {
     std::vector<std::pair<std::string, std::string>> queries = read_fasta(sample_fasta_fname, MIN_LENGTH, MAX_LENGTH);
     std::vector<std::pair<std::string, std::string>> refs = read_fasta(ref_fasta_fname, MIN_LENGTH, MAX_LENGTH);
-    StateData sd(MAX_LENGTH*2 + 1);
+    size_t max_seq_len = 0;
+    for (int si = 0; si < refs.size(); ++si) {
+        max_seq_len = std::max(max_seq_len, refs[si].second.length());
+    }
+    for (int si = 0; si < refs.size(); ++si) {
+        max_seq_len = std::max(max_seq_len, queries[si].second.length());
+    }
+    StateData sd(max_seq_len*2 + 1);
     for (auto& p1 : queries) {
         for (auto& p2 : refs) {
             int rowsize = p1.second.length() + p2.second.length() + 1;
             sd.init_state_quintuple(p1.second.length(), p2.second.length());
             sd.init_state_array(rowsize);
-            int mm_ind = 5*rowsize + p2.second.length();
-            int NM_ind = 8*rowsize + p2.second.length();
-            int NN_ind = 11*rowsize + p2.second.length();
+            int n = p2.second.length();
             bool res = calculate_dist_sd(p1.second, p2.second, sd, k, 100, false);
 //            double p = betap(state_arr[mm_ind], state_arr[NM_ind], state_arr[NN_ind], k);
-            std::cout << p1.first << "," << p2.first << "," << sd.state_quintuple[0] << "," << sd.state_arr[mm_ind] << ","
-                          << sd.state_arr[NM_ind] << "," << sd.state_arr[NN_ind] << std::endl;
+            std::cout << p1.first << "," << p2.first << "," << sd.h << "," << sd.M2[n] << ","
+                          << sd.NM2[n] << "," << sd.NN2[n] << std::endl;
         }
     }
 }
