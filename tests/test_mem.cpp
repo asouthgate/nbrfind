d34f83b9212d4bdc83948cca7c60ee3681e70b5f
j <joelsouthgate@googlemail.com>
2020-12-16 19:31:04 +0000

    imports

--- tests/test_mem.cpp
+++ tests/test_mem.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<assert.h>
 #include"uk2.hpp"
+#include"seqio.hpp"
 
 int main(int argc, char* argv[]) {
     std::cerr << "testing" << std::endl; 
+    std::string S = "NATATTNNATANANAAAATTTTTN";
+    std::pair<std::vector<int>, std::string> res = strip_Ns(S);
+    for (int j = 0; j < res.first.size(); ++j) {
+        assert(res.second[j] == S[res.first[j]]);
+    }
 }
