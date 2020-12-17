89feaaf8e91feb2e85a0ce52de16dcdeaf02f525
j <joelsouthgate@googlemail.com>
2020-12-17 16:24:09 +0000

    function for stripping Ns from a sequence

--- src/seqio.cpp
+++ src/seqio.cpp
@@ -6,6 +6,22 @@
 #include <vector>
 #include "seqio.hpp"
 
+std::pair<std::vector<int>, std::string> strip_Ns(std::string& S) {
+    std::string resS  = "";
+    std::vector<int> resinds;
+    for (int i = 0; i < S.length(); ++i) {
+        if (S[i] != 'N') {
+            resinds.push_back(i);
+            resS += S[i];
+        }
+        else if (i > 0 && S[i-1] !='N')  {
+            resS += "!";
+            resinds.push_back(-1);            
+        }
+    }
+    return std::pair<std::vector<int>, std::string>(resinds, resS);
+}
+
 std::vector<std::pair<std::string, std::string>> read_fasta(std::string fasta_fname, int min_len, int max_len) {
     std::cerr << "Reading " << fasta_fname << std::endl;
     std::vector<std::pair<std::string,std::string>> records;
