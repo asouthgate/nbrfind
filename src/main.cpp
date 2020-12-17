cb610587e5584f0083bca8199be16145692307a8
j <joelsouthgate@googlemail.com>
2020-12-17 16:23:40 +0000

    slide CLA

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 
 int main(int argc, char* argv[]) {
     int snpmax = atoi(argv[3]);
+    int max_slide = atoi(argv[4]);
     DistCalculator dc = DistCalculator();
-    dc.query_samples_against_refs(argv[1], argv[2], snpmax);
+    dc.query_samples_against_refs(argv[1], argv[2], snpmax, max_slide);
 }
 
